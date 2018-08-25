730d6f37b8d5d57747407a4010c6d00702c1f21b
Bhavin Nandani <nandanibhavin@gmail.com>
2018-08-25 21:21:47 +0530

    Configuration file

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+	session_start();
+
+	
+	require_once __DIR__ . '/vendor/autoload.php';
+	
+	$FB = new \Facebook\Facebook([
+		'app_id' => '{Your facebook id here}',
+		'app_secret' => '{Your facebook app secret}',
+		'default_graph_version' => 'v3.1'
+	]);
+
+	$helper = $FB->getRedirectLoginHelper();
+?>
